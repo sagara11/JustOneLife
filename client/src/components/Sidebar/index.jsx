1d32c1e716ec660b63d68d3a83c5daffc06de845
phongth-1074 <tran.hung.phong@sun-asterisk.com>
2022-01-21 14:37:05 +0700

    Fix sidebar on 16:9 ratio

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -15,75 +15,79 @@ function Sidebar() {
     <>
       <div className="sidebar__wrapper">
         <div className="sidebar__list-item">
-          <NavLink
-            to={"/"}
-            exact
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <ImHome className="sidebar__icon" />
-          </NavLink>
-          {/* Patient icons */}
-          <NavLink
-            to={`/medical-record/${currentUser.publicAddress}`}
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <MdDocumentScanner className="sidebar__icon" />
-          </NavLink>
-          <NavLink
-            to={`/vaccination-certificate/${currentUser.publicAddress}`}
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <AiFillSafetyCertificate className="sidebar__icon" />
-          </NavLink>
-          {/* Doctor icons */}
-          <NavLink
-            to="/medical-records"
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <IoDocumentsSharp className="sidebar__icon" />
-          </NavLink>
-          <NavLink
-            to="/vaccination-certificates"
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <AiFillSafetyCertificate className="sidebar__icon" />
-          </NavLink>
-          {/* Manager icons */}
-          <NavLink
-            to="/doctors"
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <BsPeopleFill className="sidebar__icon" />
-          </NavLink>
-          {/* Admin icons */}
-          <NavLink
-            to="/managers"
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <BsFillPersonPlusFill className="sidebar__icon" />
-          </NavLink>
-          <NavLink
-            to="/settings"
-            className={(isActive) =>
-              "sidebar__item" + (isActive ? " active-item" : "")
-            }
-          >
-            <IoSettingsSharp className="sidebar__icon" />
-          </NavLink>
+          <div className="sidebar-top">
+            <NavLink
+              to={"/"}
+              exact
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <ImHome className="sidebar__icon" />
+            </NavLink>
+            {/* Patient icons */}
+            <NavLink
+              to={`/medical-record/${currentUser.publicAddress}`}
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <MdDocumentScanner className="sidebar__icon" />
+            </NavLink>
+            <NavLink
+              to={`/vaccination-certificate/${currentUser.publicAddress}`}
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <AiFillSafetyCertificate className="sidebar__icon" />
+            </NavLink>
+            {/* Doctor icons */}
+            <NavLink
+              to="/medical-records"
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <IoDocumentsSharp className="sidebar__icon" />
+            </NavLink>
+            <NavLink
+              to="/vaccination-certificates"
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <AiFillSafetyCertificate className="sidebar__icon" />
+            </NavLink>
+            {/* Manager icons */}
+            <NavLink
+              to="/doctors"
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <BsPeopleFill className="sidebar__icon" />
+            </NavLink>
+            {/* Admin icons */}
+            <NavLink
+              to="/managers"
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <BsFillPersonPlusFill className="sidebar__icon" />
+            </NavLink>
+          </div>
+          <div className="sidebar-bottom">
+            <NavLink
+              to="/settings"
+              className={(isActive) =>
+                "sidebar__item" + (isActive ? " active-item" : "")
+              }
+            >
+              <IoSettingsSharp className="sidebar__icon" />
+            </NavLink>
+          </div>
         </div>
       </div>
     </>
