0bebf2f61190003dcc0fea71092217d055d8cd43
phongth-1074 <tran.hung.phong@sun-asterisk.com>
2022-01-25 00:58:07 +0700

    Jol32: Manager Authorize using Web3

--- client/src/features/manager/AddForm.jsx
+++ client/src/features/manager/AddForm.jsx
@@ -1,33 +1,43 @@
 import React from "react";
 import "./styles.scss";
-import {useForm} from "react-hook-form";
-import {ErrorMessage} from "@hookform/error-message";
+import { useForm } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
+import { useDispatch, useSelector } from 'react-redux';
+import { setRoleManager } from '../authorization/authorizationSlice';
+import { globalState } from '../global/globalSlice';
 
-function AddManagerForm() {
+const AddManagerForm = () => {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: {errors},
   } = useForm();
+  const dispatch = useDispatch();
+  const {web3, accounts, currentUser} = useSelector(globalState);
 
   const onSubmit = (data) => {
-    console.log(data);
+    let { address } = data;
+    dispatch(setRoleManager({web3, accounts, currentUser, address}));
+    setValue("address", "");
   };
 
   return (
-    <form className="add-manager-form" onSubmit={handleSubmit(onSubmit)} id="add-manager-form">
-      <input
-        {...register("address", {required: "This is required."})}
-       className="form-control"
-       placeholder="New manager address..."
-      />
-      <ErrorMessage
-          errors={errors}
-          name="address"
-          render={({message}) => <p>{message}</p>}
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="add-manager-form" id="add-manager-form">
+        <input
+          {...register("address", {required: "This is required."})}
+          className="form-control"
+          placeholder="New manager address..."
         />
-      <button type="button" className="btn btn-primary">ADD</button>
-    </form>
+        <ErrorMessage
+            errors={errors}
+            name="address"
+            render={({message}) => <p>{message}</p>}
+          />
+        <button className="btn btn-primary">ADD</button>
+      </form>
+    </>
   );
 }
 export default AddManagerForm;
--- contracts/Authorize.sol
+++ contracts/Authorize.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 contract Authorize is Ownable, AccessControl {
   bytes32 public constant MANAGER_ROLE = keccak256("MANAGER");
   bytes32 public constant PATIENT_ROLE = keccak256("PATIENT");
+  bytes32 public constant DOCTOR_ROLE = keccak256("DOCTOR");
 
   constructor() {
     _grantRole(DEFAULT_ADMIN_ROLE, _msgSender());
@@ -20,5 +21,22 @@ contract Authorize is Ownable, AccessControl {
 
   function setPatient(address userAddress) public {
     _grantRole(PATIENT_ROLE, userAddress);
+  }
+
+  function getRole(address userAddress) external view returns (uint[] memory) {
+    uint[] memory roleArray = new uint[](4);
+    if (hasRole(PATIENT_ROLE, userAddress)) {
+      roleArray[0] = 1;
+    }
+    if (hasRole(DOCTOR_ROLE, userAddress)) {
+      roleArray[1] = 1;
+    }
+    if (hasRole(MANAGER_ROLE, userAddress)) {
+      roleArray[2] = 1;
+    }
+    if (hasRole(DEFAULT_ADMIN_ROLE, userAddress)) {
+      roleArray[3] = 1;
+    }
+    return roleArray;
   }
 }
