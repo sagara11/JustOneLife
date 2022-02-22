d1c394fc191f467f5550a367c25b945e9d143e6b
Nguyễn Minh Thông <thiensangkdtt@gmail.com>
2022-02-22 21:23:46 +0700

    JOL-46:Contract: Medical Record file relativity

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Authorize = artifacts.require("./Authorize.sol");
+var MedicalRecord = artifacts.require("./MedicalRecord.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Authorize);
+  deployer.deploy(MedicalRecord, Authorize.address);
 };
