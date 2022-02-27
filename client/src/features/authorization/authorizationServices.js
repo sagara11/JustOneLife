e865fde69f1e5150ac825a5e6f2d0dbda962e739
longcn <nngoclongg1999@gmail.com>
2022-02-27 10:04:22 +0700

    Fix set patient

--- client/src/features/authorization/authorizationServices.js
+++ client/src/features/authorization/authorizationServices.js
@@ -1,26 +1,32 @@
-const authorizationServices = {
-  handleSetRole: async ({web3, accounts, currentUser}, AuthorizeContract) => {
-    const networkId = await web3.eth.net.getId();
+import AuthorizeContract from "../../contracts/Authorize.json";
+
+function authorizationServices(params) {
+  this.web3 = params.web3;
+  this.accounts = params.accounts;
+  this.currentUser = params.currentUser;
+
+  this.setPatientRole = async () => {
+    const networkId = await this.web3.eth.net.getId();
     const deployedNetwork = AuthorizeContract.networks[networkId];
-    const authorizeContract = new web3.eth.Contract(
+    const instance = new this.web3.eth.Contract(
       AuthorizeContract.abi,
       deployedNetwork && deployedNetwork.address
     );
-    const roleHash = web3.utils.keccak256(process.env.REACT_APP_ROLE_PATIENT);
-    const hasRole = await authorizeContract.methods
-      .hasRole(roleHash, currentUser.publicAddress)
-      .call({from: accounts[0]});
-    console.log("has Patient role: ", hasRole);
+    const roleHash = this.web3.utils.keccak256(
+      process.env.REACT_APP_ROLE_PATIENT
+    );
+    let hasRole = await instance.methods
+      .hasRole(roleHash, this.currentUser.publicAddress)
+      .call({from: this.accounts[0]});
     if (!hasRole) {
-      await authorizeContract.methods
-        .setPatient(currentUser.publicAddress)
-        .send({from: accounts[0]});
-      console.log(`User ${accounts[0]} has been set to Patient role`);
+      await instance.methods
+        .setPatient(this.currentUser.publicAddress)
+        .send({from: this.accounts[0]});
     }
-    const data = await authorizeContract.getPastEvents("RoleGranted");
-    console.log(data[0]["returnValues"]);
-    return data[0]["returnValues"];
-  },
-};
 
-module.exports = authorizationServices;
+    const data = await instance.getPastEvents("RoleGranted");
+    return data[0]["returnValues"];
+  };
+}
+
+export default authorizationServices;
