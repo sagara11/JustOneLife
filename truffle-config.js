dd9439f72e8b257b125e761ae546780493b1fb63
longcn <long.chu@gmail.com>
2022-05-04 08:34:19 +0700

    add chart to homepage

--- truffle-config.js
+++ truffle-config.js
@@ -11,9 +11,14 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
+      host: "192.168.1.4",     // Localhost (default: none)
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+    jot: {
+      host: "192.168.0.101",     // Localhost (default: none)
+      port: 10106,            // Standard Ethereum port (default: none)
+      network_id: 14333,       // Any network (default: none)
     }
   }
 };
